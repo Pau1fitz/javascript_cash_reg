7e1772e62ff84a2ed78179991695e87487e17a4e
Paul Fitzgerald <paulfitz99@gmail.com>
2015-04-06 17:51:22 +0100

    total updating, raw javascript

--- public/js/getData.js
+++ public/js/getData.js
@@ -2,7 +2,6 @@ function loadJSONDoc()
 {
   var answer;
   var xmlhttp;
-
   xmlhttp=new XMLHttpRequest();
 
 xmlhttp.onreadystatechange=function()
@@ -10,14 +9,41 @@ xmlhttp.onreadystatechange=function()
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
     answer = JSON.parse(xmlhttp.responseText)
-    console.log(answer[0]);
     var shop = answer[0].shopName;
     var address = answer[0].address;
     var phone = answer[0].phone;
     document.getElementById('shopName').innerHTML = shop;
     document.getElementById('address').innerHTML = address;
     document.getElementById('phone').innerHTML = phone;
+
+    var items = answer[0].prices[0];
+    var total = 0;
+
+    for(var index in items) {
+
+      var node = document.getElementById("orderList");
+      var p = document.createElement('p');
+      var price = items[index];
+      p.setAttribute("class", price)
+      var textnode = document.createTextNode(index + " price = " + price);
+      p.appendChild(textnode);
+      node.appendChild(p);
+      };
     }
+ var total = 0;
+
+var update = document.getElementsByTagName("p");
+for(var i=0; i< update.length; ++i) {
+    update[i].onclick = function() {
+      console.log(this.className)
+      var num = parseFloat(this.className).toFixed(2);
+      num = parseFloat(num)
+      total += num;
+      var paulis = Math.round(total*100)/100
+      document.getElementById("total").innerHTML = "Amt Due: $" + paulis;
+    }
+}
+
 }
 xmlhttp.open("GET","/items",true);
 xmlhttp.send();
@@ -26,4 +52,12 @@ xmlhttp.send();
 document.addEventListener("DOMContentLoaded", function(event) {
   loadJSONDoc();
 });
+
+
+
+
+
+
+
+
 
