975efbef9a540764c7de3b46c09d8337d060d2e5
Paul Fitzgerald <paulfitz99@gmail.com>
2015-04-06 21:11:16 +0100

    calculates change

--- public/js/getData.js
+++ public/js/getData.js
@@ -1,7 +1,17 @@
+var rounded_total;
+var change;
+var nameValue;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  loadJSONDoc();
+
+});
+
 function loadJSONDoc()
 {
   var answer;
   var xmlhttp;
+
   xmlhttp=new XMLHttpRequest();
 
 xmlhttp.onreadystatechange=function()
@@ -14,7 +24,7 @@ xmlhttp.onreadystatechange=function()
     var phone = answer[0].phone;
     document.getElementById('shopName').innerHTML = shop;
     document.getElementById('address').innerHTML = address;
-    document.getElementById('phone').innerHTML = phone;
+    document.getElementById('phone').innerHTML = "+1" + phone;
 
     var items = answer[0].prices[0];
     var total = 0;
@@ -25,23 +35,21 @@ xmlhttp.onreadystatechange=function()
       var p = document.createElement('p');
       var price = items[index];
       p.setAttribute("class", price)
-      var textnode = document.createTextNode(index + " price = " + price);
+      var textnode = document.createTextNode(index + " price = $" + price);
       p.appendChild(textnode);
       node.appendChild(p);
       };
     }
- var total = 0;
-
-var update = document.getElementsByTagName("p");
-for(var i=0; i< update.length; ++i) {
+    var total = 0;
+    var update = document.getElementsByTagName("p");
+    for(var i=0; i< update.length; ++i) {
     update[i].onclick = function() {
-      console.log(this.className)
-      var num = parseFloat(this.className).toFixed(2);
-      num = parseFloat(num)
-      total += num;
-      var paulis = Math.round(total*100)/100
-      document.getElementById("total").innerHTML = "Amt Due: $" + paulis;
-    }
+    var num = parseFloat(this.className).toFixed(2);
+    num = parseFloat(num)
+    total += num;
+    rounded_total = Math.round(total*100)/100
+    document.getElementById("total_paid").innerHTML = rounded_total;
+  }
 }
 
 }
@@ -49,10 +57,11 @@ xmlhttp.open("GET","/items",true);
 xmlhttp.send();
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  loadJSONDoc();
-});
-
+function changeDue(event){
+  var pay = document.getElementById('uniqueID').value
+  document.getElementById('change_due').innerHTML = (pay - rounded_total);
+  event.preventDefault()
+};
 
 
 
